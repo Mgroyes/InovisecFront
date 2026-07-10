d7b8118e435cbe937b4b621187066f0f45da4a51
Mgroyes <mgroyes22@gmail.com>
2026-07-10 09:04:30 -0600

    Estructura del proyecto

---
+++ src/app/components/case-detail/case-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './case-detail.component.html',
+  styleUrl: './case-detail.component.scss'
+})
+export class CaseDetailComponent {
+
+}
