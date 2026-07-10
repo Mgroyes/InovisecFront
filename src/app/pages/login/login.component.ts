ce36d15a06be3c5cea5987bba5c2fcad20cfa6f4
Mgroyes <mgroyes22@gmail.com>
2026-07-10 10:41:37 -0600

    loguinFuncional

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,23 +1,118 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  constructor(
-    private router: Router
-  ){}
+  username = '';
+  password = '';
+  remember = false;
 
-  login(){
+  // Credenciales quemadas
+  private readonly USER = 'admin';
+  private readonly PASS = '123456';
 
-    this.router.navigate(['/dashboard']);
+  constructor(private router: Router) {}
+
+  login(): void {
+
+    if (!this.username || !this.password) {
+
+      Swal.fire({
+       icon: 'warning',
+
+  title: 'Campos requeridos',
+
+  text: 'Ingrese usuario y contraseña.',
+
+  customClass: {
+
+    popup: 'inovisec-popup',
+
+    title: 'inovisec-title',
+
+    htmlContainer: 'inovisec-text',
+
+    confirmButton: 'inovisec-button'
+
+  },
+
+  buttonsStyling: false
+      });
+
+      return;
+    }
+
+    if (
+      this.username === this.USER &&
+      this.password === this.PASS
+    ) {
+
+      Swal.fire({
+
+  icon: 'success',
+
+  title: 'Bienvenido',
+
+  text: 'Inicio de sesión correcto.',
+
+  timer: 1200,
+
+  showConfirmButton: false,
+
+  customClass: {
+
+    popup: 'inovisec-popup',
+
+    title: 'inovisec-title',
+
+    htmlContainer: 'inovisec-text'
+
+  }
+
+}).then(() => {
+
+  this.router.navigate(['/dashboard']);
+
+});
+
+      return;
+    }
+
+    Swal.fire({
+      icon: 'error',
+
+  title: 'Acceso denegado',
+
+  text: 'Usuario o contraseña incorrectos.',
+
+  customClass: {
+
+    popup: 'inovisec-popup',
+
+    title: 'inovisec-title',
+
+    htmlContainer: 'inovisec-text',
+
+    confirmButton: 'inovisec-button'
+
+  },
+
+  buttonsStyling: false
+
+    });
 
   }
 
