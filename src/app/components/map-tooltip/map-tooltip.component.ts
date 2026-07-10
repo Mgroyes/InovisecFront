2d510002ceadf3aeb67f7d4b533ea0a16a1c7663
Mgroyes <mgroyes22@gmail.com>
2026-07-10 11:19:12 -0600

    componentesNuevos

---
+++ src/app/components/map-tooltip/map-tooltip.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-tooltip',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-tooltip.component.html',
+  styleUrl: './map-tooltip.component.scss'
+})
+export class MapTooltipComponent {
+
+}
